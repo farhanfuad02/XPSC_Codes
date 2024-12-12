119f3a530a49caf5a2ad80fe804400166e4819fb
farhanfuad02 <farhanf.official.02@gmail.com>
2024-12-12 20:48:02 +0600

    Add files via upload

---
+++ Week 7/12.12.2024/C_Increasing_Sequence_with_Fixed_OR.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        int maxbit = __lg(n);
+        deque<ll> ans;
+        for (int k = 0; k <= __lg(n); k++)
+        {
+            if ((n >> k) & 1)
+            {
+                ll value = n - (1LL << k);
+                if(value > 0)
+                {
+                    ans.push_front(value);
+                }
+            }
+        }
+        ans.push_back(n);
+        cout << ans.size() << endl;
+        for (auto val : ans)
+        {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
