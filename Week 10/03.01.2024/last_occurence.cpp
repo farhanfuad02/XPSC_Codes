b219cd2517aa87e1e171eccb6fbea95945a06df3
farhanfuad02 <farhanf.official.02@gmail.com>
2025-01-03 23:27:41 +0600

    Add files via upload

---
+++ Week 10/03.01.2024/last_occurence.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n,q;
+    cin >> n >> q;
+    vector<int>v(n);
+    for(int i=0; i<n; i++)
+    {
+        cin >> v[i];
+    }
+
+    for(int i=0; i<q; i++)
+    {
+        int key,l=0,r=n-1,mid,ans=-1;
+        cin >> key;
+        while(l<=r)
+        {
+            mid = l+(r-1)/2;
+            if(key==v[mid])
+            {
+                ans = mid;
+                r = mid+1;
+            }
+            else if(key<v[mid])
+            {
+                r = mid-1;
+            }
+            else
+            {
+                l = mid+1;
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
